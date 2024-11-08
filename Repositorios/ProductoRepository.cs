a513aed6b5a438a90b8c9f4230e76687583a495c
marianogk <marianokpo7@gmail.com>
2024-11-08 19:03:50 -0400

    repositorio completo

--- Repositorios/ProductoRepository.cs
+++ Repositorios/ProductoRepository.cs
@@ -1,38 +1,52 @@
 public class ProductoRepository : IRepository
 {
+    private List<Producto> productos;
 
-    public void Create(Producto producto)
+    public ProductoRepository()
     {
-
+        productos = new List<Producto>();
+    }
+    public void Insert(Producto producto)
+    {
+        productos.Add(producto);
     }
 
     public void Update(Producto producto, int idProducto)
     {
-
+        var productoBuscado = productos.FirstOrDefault(p => p.IdProducto == idProducto);
+        if (productoBuscado != null)
+        {
+            productoBuscado.Descripcion = producto.Descripcion;
+            productoBuscado.Precio = producto.Precio;
+        }
     }
-    public List<Producto> GetAll()
+    public List<Producto> FindAll()
     {
-        return List<Producto>();
+        return productos;
     }
-    public Producto GetById(int idProducto)
+    public Producto FindById(int idProducto)
     {
-        Producto producto;
-        return producto;
+        return productos.FirstOrDefault(p => p.IdProducto == idProducto);
     }
 
 
-    public void Delete(Producto producto, int idProducto)
+    public void Delete(int idProducto)
     {
-
+        var producto = productos.FirstOrDefault(p => p.IdProducto == idProducto);
+        if (producto != null)
+        {
+            productos.Remove(producto);
+        }
     }
+
 }
 
 public interface IRepository
 {
-    
+
     Producto FindById(int idProducto);
-    IRepository FindAll();
+    List<Producto> FindAll();
     void Insert(Producto producto);
-    void Update(Producto producto);
+    void Update(Producto producto, int idProducto);
     void Delete(int idProducto);
 }
--- obj/Debug/net8.0/tl2-tp5-2024-marianogk.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp5-2024-marianogk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp5-2024-marianogk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54c8742ae93a6b1014d1d65c9087a2394d312318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d35973cfbad629f97f5051700e06a46ac8f2da3")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp5-2024-marianogk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp5-2024-marianogk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
